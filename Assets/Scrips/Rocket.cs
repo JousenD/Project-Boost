adea593c97938dc0bbea7d1de2b32d657a72e14f
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2018-07-27 20:21:45 +0200

    Thrust audio added

---
+++ Assets/Scrips/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcesInput();
+	}
+
+    private void ProcesInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            rigidbody.AddRelativeForce(Vector3.up);
+            print("Thrusting");
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            print("Rotating left");
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            transform.Rotate(-Vector3.forward);
+            print("Rotating right");
+        }
+    }
+}
